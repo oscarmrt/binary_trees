3c908320f871f034d03e49b53d07c671ba5a13df
oscarmrt <oscarmrt@hotmail.com>
2020-03-12 19:29:25 +0000

    modified file 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,6 +20,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	new_node->parent = parent;
 	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
