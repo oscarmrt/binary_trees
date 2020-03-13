209c5eb542cb5d63dd5cdbc76703e56371e7cdd0
oscarmrt <oscarmrt@hotmail.com>
2020-03-13 17:34:38 +0000

    modified file 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -34,7 +34,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-		return (1);
-	else
-		return (0);
+	{
+		if (binary_tree_height(tree->left) && binary_tree_height(tree->right))
+			return (1);
+	}
+	return (0);
 }
