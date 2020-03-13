a3f26ec8929bacb0c50407b4d34b1090de5688c6
oscarmrt <oscarmrt@hotmail.com>
2020-03-13 18:40:17 +0000

    modified file 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node->parent == NULL || node == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	if (node->parent->left == node)
 		return (node->parent->right);
